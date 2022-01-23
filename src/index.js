4e6012dcecb20d258f176c78291f583fcc9d4595
AdetunjiA <trinity0305@yahoo.com>
2022-01-23 11:48:01 -0700

    css integration

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
 
-// import "./styles.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Weather Search Engine</h1>
-        <Weather />
-      </div>
+      <header className="App-header">
+        <div>
+          <h1>Weather Search Engine</h1>
+          <Weather />
+        </div>
+      </header>
     </div>
   );
 }
